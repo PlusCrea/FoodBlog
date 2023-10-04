12ef174793779192d96281196ddbaf4a09974fad
Ali Yakar <yakarali@gmail.com>
2023-10-03 21:58:21 -0400

    ga update

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './css/style.css'
 import { Inter } from 'next/font/google'
 import Header from './layout/header'
 import Footer from './layout/footer'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-TQHRZCM7KP" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-TQHRZCM7KP');
+        `}
+      </Script>
         <Header/>
         {children}
         <Footer/>
